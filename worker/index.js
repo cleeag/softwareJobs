7d89be0fcb4a02783385144c61b7714e17fffab6
cleeag <jack0506000@gmail.com>
2020-10-02 00:41:17 -0700

    Initial commit

---
+++ worker/index.js
@@ -0,0 +1,6 @@
+var CronJob = require('cron').CronJob;
+
+const fetchGithub = require('./tasks/fetch-github')
+
+
+new CronJob('* * * * *', fetchGithub, null, true, 'America/Los_Angeles');
---
+++ worker/tasks/fetch-github.js
@@ -0,0 +1,55 @@
+
+
+var fetch = require('node-fetch');
+
+var redis = require("redis"),
+    client = redis.createClient();
+
+
+const { promisify } = require("util");
+// const getAsync = promisify(client.get).bind(client);
+const setAsync = promisify(client.set).bind(client);
+
+const baseURL = "https://jobs.github.com/positions.json"
+
+async function fetchGithub(){
+
+    let resultCount = 1, onPage = 0;
+    const allJobs = [];
+
+    while(resultCount > 0){
+        const res = await fetch(`${baseURL}?page=${onPage}`);
+        const jobs = await res.json();
+        allJobs.push(...jobs);
+        resultCount = jobs.length;
+        console.log("got", jobs.length, 'jobs');
+        onPage++;
+    }
+
+    console.log("got", allJobs.length, 'jobs');
+
+    // filter algo
+    const jrJobs = allJobs.filter(job => {
+        const jobTitle = job.title.toLowerCase();
+        let isJunior = true;
+
+        // algo logic
+        if(
+            jobTitle.includes('senior') || 
+            jobTitle.includes('manager') ||
+            jobTitle.includes('sr.') ||
+            jobTitle.includes('architect')
+        ){
+            return false
+        }
+        return true
+    })
+    console.log("filtered down to", jrJobs.length, 'jobs');
+
+
+    // set in redis
+    const success = await setAsync('github', JSON.stringify(jrJobs));
+    console.log({success});
+}
+// fetchGithub();
+module.exports = fetchGithub;
